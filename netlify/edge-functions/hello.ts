f4d6198395ffc587ec72d75868bb32f19d83190b
jake champion <jake.champion@netlify.com>
2026-02-13 11:19:15 +0000

    init

---
+++ netlify/edge-functions/hello.ts
@@ -0,0 +1,12 @@
+import { getStore } from "@netlify/blobs";
+import type { Context } from "@netlify/edge-functions";
+
+export default async (req: Request, context: Context) => {
+  // Extract key from URL.
+  const { key } = context.params;
+
+  const uploads = getStore("file-uploads");
+  await uploads.delete(key);
+
+  return new Response("Blob has been deleted");
+};
