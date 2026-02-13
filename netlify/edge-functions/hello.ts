6e1ecd5201b04b9daceb743ddbd697cbf2509625
jake champion <jake.champion@netlify.com>
2026-02-13 11:29:48 +0000

    meow

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -4,6 +4,9 @@ import type { Context } from "@netlify/edge-functions";
 export default async (req: Request, context: Context) => {
 
   const uploads = getStore("file-uploads");
+
+  await uploads.set('meow', 'meow')
+
   await uploads.delete('meow');
 
   return new Response("Blob has been deleted");
